3a5ffc7c76887211304914f83d51d0a3f6c72d8a
Romuald Quantin <romu@soundstep.com>
2022-09-01 10:04:13 +0100

    Added script for 3 pr.

---
+++ chore-create-3-pr.sh
@@ -0,0 +1,79 @@
+#!/usr/bin/env bash
+
+# require:
+# https://cli.github.com/
+# https://www.npmjs.com/package/random-word-slugs
+
+NC='\033[0m'
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+CYAN='\033[0;36m'
+# YELLOW='\033[0;33m'
+
+COUNT_PR=0
+
+if ! command -v gh &> /dev/null; then
+    echo -e "${RED}Please install https://cli.github.com/${NC}"
+    exit 1
+elif ! command -v yawg &> /dev/null; then
+    echo -e "${RED}Please run \"npm install -g yawg\"${NC}"
+    exit 1
+fi
+
+function pr_name {
+    yawg --delimiter='-' --minWords=4  --maxWords=4
+}
+
+function timestamp {
+    date +"%Y-%m-%d_%s%N"
+}
+
+function check_git_dirty {
+    if [[ $(git diff --stat) != '' ]]; then
+        echo -e "${RED}Git is dirty, please commit${NC}"
+        exit 1
+    else
+        echo "Git is clean"
+    fi
+}
+
+function create_pr {
+    (( COUNT_PR++ )) || true
+    check_git_dirty
+    FILE_NAME=$1
+    CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+    PR_NAME="$COUNT_PR-$(pr_name)"
+    echo -e "${CYAN}Creating pull request named $PR_NAME${NC}"
+    # create branch
+    git checkout -b "$PR_NAME"
+    # edit files
+    printf "Hello, %s\n" "$NAME"
+    printf "\n// Some changes at %s\n" "$(timestamp)" >> "packages/fe-package1/$FILE_NAME"
+    printf "\n// Some changes at %s\n" "$(timestamp)" >> "packages/fe-package2/$FILE_NAME"
+    printf "\n// Some changes at %s\n" "$(timestamp)" >> "packages/fe-package3/$FILE_NAME"
+    printf "\n// Some changes at %s\n" "$(timestamp)" >> "apps/fe-spa/$FILE_NAME"
+    printf "\n// Some changes at %s\n" "$(timestamp)" >> "apps/fe-website/$FILE_NAME"
+    # push to remote
+    git add "packages/fe-package1/$FILE_NAME"
+    git add "packages/fe-package2/$FILE_NAME"
+    git add "packages/fe-package3/$FILE_NAME"
+    git add "apps/fe-spa/$FILE_NAME"
+    git add "apps/fe-website/$FILE_NAME"
+    git commit -m "Changes added"
+    git push origin "$PR_NAME"
+    # create release plan
+    # pnpm release-plan -- create --output --no-git-checks
+    # git add .release-plan
+    # git commit -m "Release plan created"
+    # git push origin "$PR_NAME"
+    # create pull request
+    gh pr create --title "$PR_NAME" --body "Generated pull request"
+    # back in initial branch
+    git checkout "$CURRENT_BRANCH"
+}
+
+create_pr file.txt
+create_pr file.txt
+create_pr file.txt
+
+echo -e "${GREEN}Successful${NC}"
